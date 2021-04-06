55e7cd0d51a32e8db0ca60d69a29854df3d966c1
Allan Jude <allan@klarasystems.com>
2021-04-06 21:39:39 +0000

    Don't allow put_nvlist() to panic the system
    Signed-off-by: Allan Jude <allan@klarasystems.com>

--- module/zfs/zfs_ioctl.c
+++ module/zfs/zfs_ioctl.c
@@ -1355,12 +1355,15 @@ put_nvlist(zfs_cmd_t *zc, nvlist_t *nvl)
 	if (nvl == NULL)
 		return (SET_ERROR(EINVAL));
 
-	size = fnvlist_size(nvl);
+	if (nvlist_size(nvl, &size, NV_ENCODE_NATIVE))
+		return (SET_ERROR(EINVAL));
 
 	if (size > zc->zc_nvlist_dst_size) {
 		error = SET_ERROR(ENOMEM);
 	} else {
-		packed = fnvlist_pack(nvl, &size);
+		if (nvlist_pack(nvl, &packed, &size, NV_ENCODE_NATIVE,
+		    KM_SLEEP))
+			return (SET_ERROR(EINVAL));
 		if (ddi_copyout(packed, (void *)(uintptr_t)zc->zc_nvlist_dst,
 		    size, zc->zc_iflags) != 0)
 			error = SET_ERROR(EFAULT);
